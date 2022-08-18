daa2b398f4610fc0ed6291e4b109c3fa06717b15
Ajay <ajaym050199@gmail.com>
2022-08-18 17:41:40 +0530

    adding initial files

---
+++ assets/js/Data.js
@@ -0,0 +1,90 @@
+let shopItemsData = [
+  {
+    id: "featured-1",
+    name: "Air Max 1",
+    price: 15,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/featured1.png",
+    detail: "product.html"
+  },
+  {
+    id: "featured-2",
+    name: "Air Base 2",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/featured2.png",
+    detail: "product.html"
+  },
+  {
+    id: "featured-3",
+    name: "Newmara",
+    price: 80,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/featured3.png",
+    detail: "product.html"
+  },
+  {
+    id: "product-1",
+    name: "Shox",
+    price: 15,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/women1.png",
+    detail: "product.html"
+  },
+  {
+    id: "product-2",
+    name: "Tiempo",
+    price: 13,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/women2.png",
+    detail: "product.html"
+  },
+  {
+    id: "product-3",
+    name: "Fuelband",
+    price: 18,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/women3.png",
+    detail: "product.html"
+  },
+  {
+    id: "product-4",
+    name: "Hypervenom",
+    price: 16,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/women4.png",
+    detail: "product.html"
+  },
+  {
+    id: "new-1",
+    name: "Blazers",
+    price: 19,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/new1.png",
+    detail: "product.html"
+  },
+  {
+    id: "new-2",
+    name: "Grind",
+    price: 15,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/new2.png",
+    detail: "product.html"
+  },
+  {
+    id: "new-3",
+    name: "CTR360 Maestri",
+    price: 17,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/new3.png",
+    detail: "product.html"
+  },
+  {
+    id: "new-4",
+    name: "NikeId",
+    price: 15,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "assets/images/new4.png",
+    detail: "product.html"
+  },
+];
---
+++ assets/js/cart.js
@@ -0,0 +1,158 @@
+let label = document.getElementById("label");
+let ShoppingCart = document.getElementById("shopping-cart");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
+
+let generateCartItems = () => {
+  if (basket.length !== 0) {
+    return (ShoppingCart.innerHTML = basket
+      .map((x) => {
+        let { id, item } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+        return `
+      <div class="cart-item">
+        <img width="100" src=${search.img} alt="" />
+        <div class="details">
+
+          <div class="title-price-x">
+              <h4 class="title-price">
+                <p>${search.name}</p>
+                <p class="cart-item-price">$ ${search.price}</p>
+              </h4>
+              <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+          </div>
+
+          <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">${item}</div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
+
+          <h3>$ ${item * search.price}</h3>
+        </div>
+      </div>
+
+      <article class="cart__card">
+      <div class="cart__box">
+          <img src=${search.img} alt="" class="cart__img">
+      </div>
+
+      <div class="cart__details">
+          <h3 class="cart__title">${search.name}</h3>
+          <span class="cart__price">${search.price}</span>
+
+          <div class="cart__amount">
+              <div class="cart__amount-content">
+                  <span class="cart__amount-box">
+                      <i onclick="decrement(${id})" class='bx bx-minus' ></i>
+                  </span>
+
+                  <span class="cart__amount-number">${item}</span>
+
+                  <span class="cart__amount-box">
+                      <i onclick="increment(${id})" class='bx bx-plus' ></i>
+                  </span>
+              </div>
+
+              <i onclick="removeItem(${id})" class='bx bx-trash-alt cart__amount-trash' ></i>
+          </div>
+      </div>
+  </article>
+      `;
+      })
+      .join(""));
+  } else {
+    ShoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2>Cart is Empty</h2>
+    <a href="index.html">
+      <button class="HomeBtn">Back to home</button>
+    </a>
+    `;
+  }
+};
+
+generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  generateCartItems();
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+  TotalAmount();
+};
+
+let removeItem = (id) => {
+  let selectedItem = id;
+  // console.log(selectedItem.id);
+  basket = basket.filter((x) => x.id !== selectedItem.id);
+  generateCartItems();
+  TotalAmount();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let clearCart = () => {
+  basket = [];
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let TotalAmount = () => {
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((x) => {
+        let { item, id } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+
+        return item * search.price;
+      })
+      .reduce((x, y) => x + y, 0);
+    // console.log(amount);
+    label.innerHTML = `
+    <h2>Total Bill : $ ${amount}</h2>
+    <button class="checkout">Checkout</button>
+    <button onclick="clearCart()" class="removeAll">Clear Cart</button>
+    `;
+  } else return;
+};
+
+TotalAmount();
